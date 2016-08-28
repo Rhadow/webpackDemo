45ecfe3720c127e16f76336892cc56d632c26ce4
Rhadow <howard3433@gmail.com>
2016-08-28 15:17:00 +0800

    Add url and file loaders to load images and fonts, also add webpack dash board

--- app/Components/App/App.js
+++ app/Components/App/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
+import reactLogo from '../../assets/react-logo.png';
 
-const App = () => <div className="colored">Hello World!</div>;
+const App = () => (
+  <div className="colored">
+    <h1>Hello World!</h1>;>
+    <img alt="react-logo" src={reactLogo} />
+  </div>
+);
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+/* eslint-disable */
+var webpack = require('webpack');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+/* eslint-enable */
+
 module.exports = {
-    entry: './app/app.js',
+    entry: {
+        app: './app/app.js',
+        vendor: ['react']
+    },
     output: {
         path: './dist',
         publicPath: '/dist/',
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'eslint',
+                exclude: 'node_modules',
+            }
+        ],
         loaders: [
             {
                 test: /\.jsx?$/,
@@ -19,9 +34,26 @@ module.exports = {
                 test: /\.css$/,
                 exclude: 'node_modules',
                 loaders: ['style', 'css']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                exclude: 'node_modules',
+                loader: 'url',
+                query: {
+                    limit: 5000
+                }
+            },
+            {
+                test: /\.(woff|woff2|ttf|eot)(\?v=\d\.\d\.\d)?$/,
+                exclude: 'node_modules',
+                loader: 'file'
             }
         ]
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+        new DashboardPlugin()
+    ],
     resolve: {
         // Default extensions: ["", ".webpack.js", ".web.js", ".js"]
         extensions: ['', '.js', '.css'],
